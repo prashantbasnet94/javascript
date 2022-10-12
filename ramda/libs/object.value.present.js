f532137bc5370219c862836e453541c0ee52c1b2
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2022-10-11 19:58:14 -0500

    Update object.value.present.js

--- ramda/libs/object.value.present.js
+++ ramda/libs/object.value.present.js
@@ -1,63 +1,75 @@
 const firstTestCase = {
     anest1: -1,
-    bnest1:-1,
-    cls:{
-      anest2: -1,
-      bnest2: -1,
-      desk:{
-        anest3:-1,
-        bnest3: -1
-      }
+    bnest1: -1,
+    cls: {
+        anest2: -1,
+        bnest2: -1,
+        desk: {
+            anest3: -1,
+            bnest3: -1
+        }
     },
-    second:{
-      asecondnest2: null,
-      bsecondnest2: -1,
-      desk:{
-        asecondnest3:-1,
-        bsecondnest3: -1
-      }
+    second: {
+        asecondnest2: null,
+        bsecondnest2: -1,
+        desk: {
+            asecondnest3: -1,
+            bsecondnest3: -1
+        }
     },
-     third: {
-      athirdnames2: 2,
-      bthirdnames2: -1,
-      desk:{
-        athirdnames3:-1,
-        bthirdnames2: -1
-      }
-    } 
-  },
-  secondTestCase = {
-    first: null,
-    second:{
-        third: 2
-    }
-  },
-  thirdTestCase = {
-    first: null,
-    second:{
-        third: {
-            fourth: null,
-            fifth: -1
+    third: {
+        athirdnames2: 2,
+        bthirdnames2: -1,
+        desk: {
+            athirdnames3: -1,
+            bthirdnames2: -1
         }
     }
-  }
-  getchildObjs = data => data.filter(data => data && data.constructor.name === 'Object'),
-  isTruthyValue = x => x && x !== -1 && x !== "" && x.constructor.name !== 'Object',
-  {values} = Object,
-    objHasAnyValue = datas => {
+},
+    secondTestCase = {
+        first: null,
+        second: {
+            third: 2
+        }
+    },
+    thirdTestCase = {
+        first: null,
+        second: {
+            third: {
+                fourth: null,
+                fifth: -1
+            }
+        }
+    },  
+    getonlyChildObjs = data => data.filter(data => data && data.constructor.name === 'Object'),
+    isTruthyValue = x => x && x !== -1 && x !== "" && x.constructor.name !== 'Object',
+    { values } = Object,
+    objHasAnyTruthyValue = datas => {
         const
-            currentIteration = values(datas),
-            childObjs = getchildObjs(currentIteration)
-        if (typeof childObjs === 'undefined') {
+            currentObjValues = values(datas),
+            onlyChildObjs = getonlyChildObjs(currentObjValues)
+        if (typeof onlyChildObjs === 'undefined') {
             return null
         }
-        if (currentIteration.some(isTruthyValue)) {
+        if (currentObjValues.some(isTruthyValue)) {
             return true
         }
-        return childObjs.map(o => objHasAnyValue(o)).filter( o => o).length > 0
+        /*
+        
+        onlyChildObjs:[
+                { anest2: -1, bnest2: -1, desk: { anest3: -1, bnest3: -1 } },
+                {
+                    asecondnest2: 2,
+                    bsecondnest2: -1,
+                    desk: { asecondnest3: -1, bsecondnest3: -1 }
+                }
+            ]
+            using map to account for each child objs
+    */
+        return onlyChildObjs.map(o => objHasAnyTruthyValue(o)).filter(o => o).length > 0
     }
-  
-  console.log(objHasAnyValue(firstTestCase))
-  console.log(objHasAnyValue(secondTestCase))
-  console.log(objHasAnyValue(thirdTestCase))
+
+console.log(objHasAnyTruthyValue(firstTestCase))
+console.log(objHasAnyTruthyValue(secondTestCase))
+console.log(objHasAnyTruthyValue(thirdTestCase))
 
